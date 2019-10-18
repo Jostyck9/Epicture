2f23d9c89186d8fd05718ccd34b240a72ea5b5da
Jostyck9 <hugo.berthome@epitech.eu>
2019-10-18 23:49:58 +0200

    Adding SplashScreen and LoginScreen

--- app/src/main/java/com/example/epicture/Constants.kt
+++ app/src/main/java/com/example/epicture/Constants.kt
@@ -2,6 +2,14 @@ package com.example.epicture
 
 class Constants {
     companion object {
-        var clientID = BuildConfig.IMGUR_API_KEY
+        val clientID = BuildConfig.IMGUR_API_KEY
+        val urlCallBack = "https://app.getpostman.com/oauth2/callback"
+        val state = "Epicture"
+        var accessToken = ""
+        var refreshToken = ""
+        var expiresIn = ""
+        var tokenType = ""
+        var username = ""
+        var accounId = ""
     }
 }
---
+++ app/src/main/java/com/example/epicture/Network/OAuthUrlUtil.kt
@@ -0,0 +1,28 @@
+package com.example.epicture.Network
+
+import com.example.epicture.Constants
+import java.net.URL
+
+class OAuthUrlUtil {
+    fun getToken(url : String) : Boolean {
+        var listUrl = url.split("?")
+        if (listUrl.size <= 1 && listUrl[0] != Constants.urlCallBack)
+            return false
+
+        var listState = listUrl[1].split("#")
+        if (listState.size <= 1 && listState[0] != "state=" + Constants.state)
+            return false
+
+        var listRes = listState[1].split("&")
+        if (listRes.size != 6)
+            return false
+
+        Constants.accessToken = listRes[0].split("=")[1]
+        Constants.expiresIn = listRes[1].split("=")[1]
+        Constants.tokenType = listRes[2].split("=")[1]
+        Constants.refreshToken = listRes[3].split("=")[1]
+        Constants.username = listRes[4].split("=")[1]
+        Constants.accounId = listRes[5].split("=")[1]
+        return true
+    }
+}
