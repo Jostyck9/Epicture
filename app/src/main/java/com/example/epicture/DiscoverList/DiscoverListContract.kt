68e07e0f7f94fd0a56c089d70e0cb2c9d85060df
Jostyck9 <hugo.berthome@epitech.eu>
2019-10-18 11:44:07 +0200

    Adding DiscoverListContract

---
+++ app/src/main/java/com/example/epicture/DiscoverList/DiscoverListContract.kt
@@ -0,0 +1,37 @@
+package com.example.epicture.DiscoverList
+
+import android.graphics.Movie
+import com.example.epicture.Model.Gallery
+
+interface DiscoverListContract {
+
+    interface Model {
+        interface OnFinishedListener {
+            fun onFinished(galleryArrayList : List<Gallery>)
+
+            fun onFailure(t : Throwable)
+        }
+
+        fun getGalleryList(onFinishedListener : OnFinishedListener, pageNo: Int)
+    }
+
+    interface View {
+        fun showProgress()
+
+        fun hideProgress()
+
+        fun setDataToRecyclerView(galleryArrayList: List<Gallery>)
+
+        fun onResponseFailure(throwable: Throwable)
+    }
+
+    interface Presenter {
+
+        fun onDestroy()
+
+        fun getMoreData(pageNo: Int)
+
+        fun requestDataFromServer()
+
+    }
+}
