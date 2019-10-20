9d9cba9d1ac26dc903578b798c8c2d307aca9565
Jostyck9 <hugo.berthome@epitech.eu>
2019-10-20 15:02:26 +0200

    Update GalleryDetail layout

--- app/src/main/java/com/example/epicture/Constants.kt
+++ app/src/main/java/com/example/epicture/Constants.kt
@@ -2,10 +2,10 @@ package com.example.epicture
 
 class Constants {
     companion object {
-        var clientID = "c0de6adf042c785"
-        val urlCallBack = "https://app.getpostman.com/oauth2/callback"
+        const val clientID = "c0de6adf042c785"
+        const val urlCallBack = "https://app.getpostman.com/oauth2/callback"
         const val apiUrl = "https://api.imgur.com/3/"
-        val state = "Epicture"
+        const val state = "Epicture"
         var accessToken = ""
         var refreshToken = ""
         var expiresIn = ""
